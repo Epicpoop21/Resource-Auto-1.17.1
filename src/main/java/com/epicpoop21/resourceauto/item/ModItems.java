787d15fcf7eab530dc483845a453758717c4172e
Epicpoop21 <l9w1sph@gmail.com>
2021-11-07 08:00:41 +0000

    Added a few more items

--- src/main/java/com/epicpoop21/resourceauto/item/ModItems.java
+++ src/main/java/com/epicpoop21/resourceauto/item/ModItems.java
@@ -19,6 +19,33 @@ public class ModItems {
     public static final RegistryObject<Item> STEEL_NUGGET = ITEMS.register("steel_nugget",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
 
+    public static final RegistryObject<Item> SCANDIUM = ITEMS.register("scandium",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
+
+    public static final RegistryObject<Item> YTTRIUM = ITEMS.register("yttrium",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
+
+    public static final RegistryObject<Item> TITANIUM = ITEMS.register("titanium",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
+
+    public static final RegistryObject<Item> ZIRCONIUM = ITEMS.register("zirconium",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
+
+    public static final RegistryObject<Item> HAFNIUM = ITEMS.register("hafnium",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
+
+    public static final RegistryObject<Item> CHROMIUM = ITEMS.register("chromium",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
+
+    public static final RegistryObject<Item> NIOBIUM = ITEMS.register("niobium",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
+
+    public static final RegistryObject<Item> TANTALUM = ITEMS.register("tantalum",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.RESOURCEAUTO_TAB)));
+
+
+
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
